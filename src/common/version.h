492f355d49132dfcb52d167073b82395cee5d572
José Westin <josewestin@live.com>
2012-10-31 05:34:32 -0200

    Pré-re baseado na compilação [3006] - Códigos OLD

---
+++ src/common/version.h
@@ -0,0 +1,31 @@
+// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+// Sistema para lançamento de RC's
+#define ATHENA_MAJOR_VERSION	1	// Versão principal
+#define ATHENA_MINOR_VERSION	0	// Versão secundária
+#define ATHENA_REVISION			0	// Revisão (3 finais dígitos)
+
+#define ATHENA_RELEASE_FLAG		1 // 1 = Em desenvolvimento, 0 = Estável
+#define ATHENA_OFFICIAL_FLAG	1 // 1 = Em desenvolvimento, 0 = Estável
+
+#define ATHENA_SERVER_NONE	0	// Indefinido
+#define ATHENA_SERVER_LOGIN	1	// Login server
+#define ATHENA_SERVER_CHAR	2	// Char server
+#define ATHENA_SERVER_INTER	4	// Inter server
+#define ATHENA_SERVER_MAP	8	// Map server
+
+// ATHENA_MOD_VERSION‚Íƒpƒbƒ`”Ô†‚Å‚·B
+// ‚±‚ê‚Í–³—‚É•Ï‚¦‚È‚­‚Ä‚à‹C‚ªŒü‚¢‚½‚ç•Ï‚¦‚é’ö“x‚Ìˆµ‚¢‚ÅB
+// i–ˆ‰ñƒAƒbƒvƒ[ƒh‚Ì“x‚É•ÏX‚·‚é‚Ì‚à–Ê“|‚Æv‚í‚ê‚é‚µA‚»‚à‚»‚à
+// @‚±‚Ì€–Ú‚ğQÆ‚·‚él‚ª‚¢‚é‚©‚Ç‚¤‚©‚Å‹^–â‚¾‚©‚çBj
+// ‚»‚Ì’ö“x‚Ìˆµ‚¢‚È‚Ì‚ÅAƒT[ƒo[‚É–â‚¢‡‚í‚¹‚é‘¤‚àA‚ ‚­‚Ü‚Å–ÚˆÀ’ö“x‚Ìˆµ‚¢‚Å
+// ‚ ‚ñ‚Ü‚èM—p‚µ‚È‚¢‚±‚ÆB
+// Isnapshot‚Ì‚âA‘å‚«‚È•ÏX‚ª‚ ‚Á‚½ê‡‚Íİ’è‚µ‚Ä‚Ù‚µ‚¢‚Å‚·B
+// CŒ¾Œê‚Ìd—lãAÅ‰‚É0‚ğ•t‚¯‚é‚Æ8i”‚É‚È‚é‚Ì‚ÅŠÔˆá‚¦‚È‚¢‚Å‰º‚³‚¢B
+#define ATHENA_MOD_VERSION	1249	// mod version (patch No.)
+
+#endif /* _VERSION_H_ */
